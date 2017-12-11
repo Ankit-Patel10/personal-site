0756b21dfa6b347b1dc25eade541f153886bac4b
Edwin Zhang <edwinzhang64@gmail.com>
2017-12-11 13:12:29 -0500

    Minor bug fixes

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,35 +4,6 @@ import Project from '../components/Project.js'
 
 class ProjectsPage extends React.Component {
   render() {
-    { /*
-
-    const messengerDesc =
-      <p>
-        A
-        <span className='emphasis'> Chrome extension </span>
-        built with
-        <span className='emphasis'> javascript </span>
-        and the
-        <span className='emphasis'> MathJax </span>
-        library that allows
-        <span className='emphasis'> LaTeX </span>
-        equations to be rendered directly inside
-        Facebook Messenger.
-      </p>
-
-    const ayaDesc =
-      <p>
-        A robotic head powered by a Raspberry Pi that uses
-        <span className='emphasis'> Amazon Web Services </span>
-        for image recognition and text-to-speech output.
-        Developed
-        <span className='emphasis'> Python </span>
-        backend and used
-        <span className='emphasis'> Tensorflow </span>
-        to create a custom algorithm for determining label relevancy.
-      </p>
-      */ }
-
     return (
       <div className='main projects'>
         <div className='title'>
