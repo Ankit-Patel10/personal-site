0c2ceb322c79a24ef6d40994a70fb69ad9fc877c
Edwin Zhang <edwinzhang64@gmail.com>
2017-12-22 13:06:10 -0700

    Update Resume

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 class NotFoundPage extends React.Component {
   render() {
     return (
-      <div className="main notfound">
+      <div className="main notfound home">
         <h2>Oops, this page doesn't exist!</h2>
         <Link to='/'><button className='homelink button-blue'>Home</button></Link>
       </div>
